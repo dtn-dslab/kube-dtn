f62d3354a2e919ac0c98108626e87905359988e7
yyoung <yyoung2001@gmail.com>
2023-03-26 16:10:09 +0800

    chore: update latency historgram buckets

--- daemon/metrics/latency_histograms.go
+++ daemon/metrics/latency_histograms.go
@@ -10,9 +10,9 @@ func NewLatencyHistograms() *LatencyHistograms {
 	return &LatencyHistograms{
 		Histograms: prometheus.NewHistogramVec(
 			prometheus.HistogramOpts{
-				Name:    "request_duraiion_milliseconds",
+				Name:    "kubedtnd_request_duration_milliseconds",
 				Help:    "Latency of requests in milliseconds",
-				Buckets: []float64{0, 1, 5, 10, 20, 50, 100, 200, 500},
+				Buckets: []float64{0, 1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000},
 			},
 			[]string{"method"},
 		),
