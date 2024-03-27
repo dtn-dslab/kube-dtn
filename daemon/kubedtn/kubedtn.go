7bcc9c195c281b0882ce666fcbda5538ea95790b
Ziming, Tan <albus_tan@outlook.com>
2024-03-27 13:08:24 +0000

    fix : dbg

--- daemon/kubedtn/kubedtn.go
+++ daemon/kubedtn/kubedtn.go
@@ -200,7 +200,7 @@ func GetNodesInfo(kClient kubernetes.Interface) (map[string]string, error) {
 func CreateVXLAN(vxlan_name string, vni string, remoteIP string, localIP string, dstPort string) error {
 	createCmd := exec.Command("ip", "link", "add", vxlan_name, "type", "vxlan", "id", vni, "remote", remoteIP, "local", localIP, "dstport", dstPort)
 	if err := createCmd.Run(); err != nil {
-		return fmt.Errorf("error creating VXLAN device: %v", err)
+		return fmt.Errorf("error creating VXLAN device %v: %v", err, createCmd.String())
 	}
 
 	upCmd := exec.Command("ip", "link", "set", vxlan_name, "up")
